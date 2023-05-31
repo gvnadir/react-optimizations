fe22c150a382efb71153b45b9363585ee0d99dee
gvnadir <gianlucanadir@gmail.com>
2023-05-31 19:24:21 +0200

    unoptimized operation

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import './App.css'
 export default function App() {
   const [localNumber, setLocalNumber] = useState(0);
   const [childNumber, setChildNumber] = useState(0);
+	const [arr, setArr] = useState([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
 
   useEffect(() => {
     console.log("parent render");
@@ -34,11 +35,17 @@ export default function App() {
 		setChildNumber(number)
 	}
 
+  function getLargestNumber() {
+    console.log("getLargestNumber");
+    return Math.max(...arr);
+  }
+
   return (
     <div className="App">
       <Child changeNumber={memoizedCallback} number={childNumber} />
       <button onClick={incrementLocal}>Click to increment local</button>
       <h1>local: {localNumber}</h1>
+      <h1>Largest Number: {getLargestNumber()}</h1>
     </div>
   );
 }
